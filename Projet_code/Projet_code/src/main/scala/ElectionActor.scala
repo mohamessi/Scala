c135a1a82b557ee78ce6cd69b8c90b60296851bc
mohamessi <mo-2010@live.fr>
2018-02-10 15:48:45 +0100

    election

--- Projet_code/Projet_code/src/main/scala/ElectionActor.scala
+++ Projet_code/Projet_code/src/main/scala/ElectionActor.scala
@@ -47,11 +47,77 @@ class ElectionActor (val id:Int, val terminaux:List[Terminal]) extends Actor {
 
           case Initiate => 
 
-          case ALG (list, init) => 
+          case ALG (list, init) =>
+               if (status == Passive()){
+                    status = Dummy()
+                    list.foreach(n => {
+                         if( n == (id + 1) % terminaux.length){
+                              var remote = context.actorSelection("akka.tcp://LeaderSystem" + terminaux(n).id + "@" + terminaux(n).ip + ":" + terminaux(n).port + "/user/Node/electionActor")
+                              remote ! ALG(list, init)
+                         }
 
-          case AVS (list, j) => 
+                    })
 
-          case AVSRSP (list, k) => 
+               }
+
+               if( status == Candidate()){
+                    candPred = init
+                    if(id > init) {
+                         if (candSucc == -1) {
+                              status = Waiting()
+                              var remote = context.actorSelection("akka.tcp://LeaderSystem" + terminaux(init).id + "@" + terminaux(init).ip + ":" + terminaux(init).port + "/user/Node/electionActor")
+                              remote ! AVS(list, init)
+                         }
+
+                         else {
+                              var remote = context.actorSelection("akka.tcp://LeaderSystem" + terminaux(candSucc).id + "@" + terminaux(candSucc).ip + ":" + terminaux(candSucc).port + "/user/Node/electionActor")
+                              remote ! AVSRSP(list, candPred)
+                              status = Dummy()
+                         }
+                    }
+                    if(init == id){
+                         status = Leader()
+                    }
+               }
+
+          case AVS (list, j) => {
+               if(status == Passive()){
+                    if(candPred == -1 )
+                         candSucc = j
+                    else{
+                         var remote = context.actorSelection("akka.tcp://LeaderSystem" + terminaux(candPred).id + "@" + terminaux(candPred).ip + ":" + terminaux(candPred).port + "/user/Node/electionActor")
+                         remote ! AVSRSP(list, j)
+                         status = Dummy()
+                    }
+                    }
+               if(status == Waiting()){
+                    candSucc = j
+               }
+          }
+
+          case AVSRSP (list, k) => {
+               if(status == Waiting()){
+                    if( id == k)
+                         status = Leader()
+                    else{
+                         candPred = k
+                         if(candSucc == -1){
+                              if(k < id){
+                                   status = Waiting()
+                                   var remote = context.actorSelection("akka.tcp://LeaderSystem" + terminaux(k).id + "@" + terminaux(k).ip + ":" + terminaux(k).port + "/user/Node/electionActor")
+                                   remote ! AVS(list, id)
+
+                              }
+                         }
+                         else{
+                              status = Dummy()
+                              var remote = context.actorSelection("akka.tcp://LeaderSystem" + terminaux(candSucc).id + "@" + terminaux(candSucc).ip + ":" + terminaux(candSucc).port + "/user/Node/electionActor")
+                              remote ! AVS(list, k)
+                         }
+                    }
+               }
+          }
+
 
      }
 
